526d05003b5e887a75bde62426d58efa34425bfa
Difei (Amber) Gao <dg3294@nyu.edu>
2023-09-25 17:59:19 -0400

    Drew 1st set of circles

--- sketch.js
+++ sketch.js
@@ -1,6 +1,26 @@
+let maxDiam = 200
+let minDiam = 20
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
+  background("yellow");
+  fill(0)
+  for (let xpos = 0; xpos <= width; xpos += maxDiam){
+    for (let ypos = 0; ypos <= height; ypos += maxDiam){
+      ellipse(xpos, ypos, 30, 30)
+    }
+  }
+
+
+
+
+
+
+
 }
